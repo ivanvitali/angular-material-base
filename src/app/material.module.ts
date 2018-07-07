150b546994cb16f73cc73a648ae5316e7381f1b6
Ivan Vitali <webcoder.ivan@gmail.com>
2018-07-07 19:44:36 -0300

    Proyecto inicial base con Angular Material

---
+++ src/app/material.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { } from '@angular/material';
+
+@NgModule({
+    imports: [
+
+    ],
+    exports: [
+
+    ],
+    providers: [
+
+    ]
+})
+export class MaterialModule {}
